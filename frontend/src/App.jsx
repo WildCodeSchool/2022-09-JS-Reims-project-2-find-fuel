64e1ab72c85d2e8630f969c349b1f435671f6835
Charlotte Kieffer <kindred242@gmail.Com>
2022-10-20 17:28:01 +0200

    resolved errors in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
-
 import Fuels from "@components/filter/Fuels";
 import "./App.css";
-import Buttons from "@components/filter/Buttons";
 
 function App() {
-
   return (
     <div className="App">
       <Fuels />
