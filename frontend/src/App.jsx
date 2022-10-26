9bd0cfc50fb31ce4a6cc655376740709876512c6
AlexisCode22 <durinalexis@gmail.com>
2022-10-26 10:18:10 +0200

    fix import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import NavBar from "./components/navbar/NavBar";
 import Fuels from "./components/filter/Fuels";
-import Home from "./pages/Home";
 import "./App.css";
 import FuelItems from "./components/ItemsList/FuelItems";
 
 function App() {
   return (
     <div className="App">
-      <Home />
       <NavBar />
       <FuelItems />
       <Fuels />
