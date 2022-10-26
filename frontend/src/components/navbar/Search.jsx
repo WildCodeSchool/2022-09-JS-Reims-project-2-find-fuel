9f400cdb4bd553135181b2e20c0cc745ea2b084d
Rémi Lagreze <remilagr02@gmail.com>
2022-10-26 12:11:08 +0200

    Searchbar is now updating his state on each change, and the default state is the geolocation

--- frontend/src/components/navbar/Search.jsx
+++ frontend/src/components/navbar/Search.jsx
@@ -1,7 +1,18 @@
+import React, { useState } from "react";
+
+const geolocation =
+  "https://api-adresse.data.gouv.fr/reverse/?lon=4.019932&lat=49.2571245";
 function Search() {
+  const [message, setMessage] = useState(geolocation);
+  console.log(message);
   return (
     <div className="nav-search">
-      <input className="search" type="text" placeholder="Recherche" />
+      <input
+        className="search"
+        type="text"
+        placeholder="Recherche"
+        onChange={(event) => setMessage(event.target.value)}
+      />
     </div>
   );
 }
