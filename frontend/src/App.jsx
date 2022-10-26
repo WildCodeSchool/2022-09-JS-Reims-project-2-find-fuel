a638cb307045b1dc377713767a75b7141bc683d6
AlexisCode22 <durinalexis@gmail.com>
2022-10-26 10:14:07 +0200

    import geolocation in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import Home from "./pages/Home";
 import NavBar from "./components/navbar/NavBar";
+import Home from "./pages/Home";
 import "./App.css";
+import Geolocation from "./components/geolocation/Geolocation";
 
 function App() {
+  const location = Geolocation();
   return (
     <div className="App">
       <Home />
       <NavBar />
+      {location.loaded
+        ? JSON.stringify(location)
+        : "Location data not available yet"}
     </div>
   );
 }
-
 export default App;
