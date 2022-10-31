634b29fca2035fc8b8d8e6cb3c762a00d5044ce5
wild-valentin <marlois.valentin@gmail.com>
2022-10-31 15:05:56 +0100

    add props on the search and navbar for the state city in app.js

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,16 +1,21 @@
 import "../../../style/navbar.css";
 import React from "react";
+import PropTypes from "prop-types";
 import Search from "./Search";
 import essence from "./Image/logo_find_fuel.png";
 import filter from "./Image/filter.png";
 
-function NavBar() {
+function NavBar(props) {
+  const { setVille } = props;
   return (
     <header>
       <img className="icon" src={essence} alt="The logo" />
-      <Search />
+      <Search setVille={setVille} />
       <img className="icon1" src={filter} alt="Filter Icon" />
     </header>
   );
 }
+NavBar.propTypes = {
+  setVille: PropTypes.func.isRequired,
+};
 export default NavBar;
--- frontend/src/components/navbar/Search.jsx
+++ frontend/src/components/navbar/Search.jsx
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-function Search() {
+function Search(props) {
+  const { setVille } = props;
   const [city, setCity] = useState("");
+
+  function handleClick(e) {
+    e.preventDefault();
+    setVille(e.target.city.value);
+  }
   return (
-    <div className="nav-search">
+    <form className="nav-search" onSubmit={(e) => handleClick(e)}>
       <input
+        name="city"
         value={city}
         className="search"
         type="text"
         placeholder="Recherche"
         onChange={(event) => setCity(event.target.value)}
       />
-    </div>
+    </form>
   );
 }
+Search.propTypes = {
+  setVille: PropTypes.func.isRequired,
+};
 export default Search;
