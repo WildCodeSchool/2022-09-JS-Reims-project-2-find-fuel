8b3ae399c368085292067d34925d3e7c3394768a
wild-valentin <marlois.valentin@gmail.com>
2022-11-03 14:03:20 +0100

    fix

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -2,14 +2,14 @@ import "../../../style/navbar.css";
 import React from "react";
 import PropTypes from "prop-types";
 import Search from "./Search";
+import logo from "../../assets/logo_find_fuel_white.png";
 
 function NavBar(props) {
   const { setVille } = props;
   return (
     <header>
-      <img className="icon" src={" "} alt="The logo" />
+      <img className="icon" src={logo} alt="The logo" />
       <Search setVille={setVille} />
-      <img className="icon1" src={" "} alt="Filter Icon" />
     </header>
   );
 }
